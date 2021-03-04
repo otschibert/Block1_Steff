b6fd56b16e1fce605f7856d1a7a2ae2ede64b1e9
otschibert <stefanie.kleinsasser@gmail.com>
2021-03-04 11:38:40 +0100

    Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,12 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World on day 4321");
+        System.out.println();
+        System.out.println("Juhu, ich programmiere!");
+        System.out.println();
+        System.out.print("Was ist denn da der Unterschied? ");
 
-        //asdfffff
+        System.out.println("Ich sehe keinen Unterschied... oder doch! :)");
 
     } // public static void
 
